4106d5233e12e461c64475220be7557e9012d424
AubinSaby <aubinsaby@gmail.com>
2022-01-25 11:11:33 +0100

    test

--- Portfolio/Sentinelle/Daemon.sh
+++ Portfolio/Sentinelle/Daemon.sh
@@ -2,18 +2,18 @@
 
 $var "./test.odt"
 
-FILESIZE=$(stat -c%s "$var")
+#$FILESIZE (stat -c%s $var)
 
-echo "le fichier pèse $FILESIZE"
+#echo "le fichier pèse $FILESIZE"
 
-while true
-do
-    if test $var
-    then 
-    echo "Le fichier a été modifié"
-    else
-    echo "Le fichier n'a pas été modifié"
-    fi
-
-    sleep 3;
-done
+#while true
+#do
+#    if test $var
+#    then 
+#    echo "Le fichier a été modifié"
+#    else
+#    echo "Le fichier n'a pas été modifié"
+#    fi
+#
+#    sleep 3;
+#done
