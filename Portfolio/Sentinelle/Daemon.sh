04ff7c67266a94528c299b1edaa74c2d9f4846b7
AubinSaby <aubinsaby@gmail.com>
2022-01-26 11:32:34 +0100

    suite

--- Portfolio/Sentinelle/Daemon.sh
+++ Portfolio/Sentinelle/Daemon.sh
@@ -4,6 +4,8 @@ var="/mnt/c/Users/aubin/OneDrive/Documents/GitHub/Projets/Portfolio/Sentinelle/t
 number_of_words1=`wc --word < $var`
 
 # méthode simple de comptage de mots
+# fichier qui s'écrase 
+# date et heure à faire
 
 echo "Le nombre de mots est : $number_of_words1"
 
@@ -18,10 +20,10 @@ do
         if test "$number_of_words1" -gt "$number_of_words2"
         then
             nb_supp=${number_of_words1}-${number_of_words2}
-            echo "Le fichier a été modifié avec suppression de $nb_supp mots"
+            echo "Le fichier a été modifié avec suppression de $nb_supp mots le $date à $heure" > Resu
         else
             nb_ajout=$number_of_words2-$number_of_words1
-            echo "Le fichier a été modifié avec ajout de $nb_ajout mots"
+            echo "Le fichier a été modifié avec ajout de $nb_ajout mots le $date à $heure" > Resu
         fi
     fi
 
