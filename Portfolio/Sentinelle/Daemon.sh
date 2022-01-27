6a8bc045c709145dfe6fb4e1309a26ade252bea9
AubinSaby <aubinsaby@gmail.com>
2022-01-27 11:27:06 +0100

    suite

--- Portfolio/Sentinelle/Daemon.sh
+++ Portfolio/Sentinelle/Daemon.sh
@@ -1,13 +1,17 @@
 #/mnt/c/Windows/System32
 
-var="/mnt/c/Users/aubin/OneDrive/Documents/GitHub/Projets/Portfolio/Sentinelle/test"
-number_of_words1=`wc --word < $var`
+# avoir une varibale originelle du nombre de mots/cara/lignes
+# mots avec s ou non
+# faire un switch 
+# mélanger les méthodes
+# rajouter le comptage de lignes
 
-w1=$number_of_words1
+var="/mnt/c/Users/aubin/OneDrive/Documents/GitHub/Projets/Portfolio/Sentinelle/test"
 
 # méthode simple de comptage de mots
 
-# mots avec s ou non
+number_of_words1=`wc -w < $var`
+w1=$number_of_words1
 
 echo "Le nombre de mots est : $number_of_words1"
 
@@ -35,4 +39,37 @@ do
     sleep 2;
 
     w1=$number_of_words2
+done
+
+# Méthode plus précise avec les caractères
+
+number_of-cara1=`wc -m < $var`
+c1=$number_of_cara1
+
+echo "Le nombre de caractères est : $number_of_cara1"
+
+while true
+do
+    number_of_cara2=`wc -m < $var`
+
+    c2=$number_of_cara2
+
+    if test "$c1" -eq "$c2"
+    then 
+    echo "Le fichier n'a pas été modifié"
+    else
+        heure=$(date +%H:%M:%S)
+        if test "$c1" -gt "$c2"
+        then
+            nb_supp=$(($c1-$c2))
+            echo "Le fichier a été modifié avec suppression de $nb_supp caractères le `date +%x` à $heure" >> Resu
+        else
+            nb_ajout=$(($c2-$c1))
+            echo "Le fichier a été modifié avec ajout de $nb_ajout caractères le `date +%x` à $heure" >> Resu
+        fi
+    fi
+
+    sleep 2;
+
+    c1=$number_of_cara2
 done
