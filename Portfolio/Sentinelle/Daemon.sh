bca7dbb87704605db95d57262e746615169614b9
AubinSaby <aubinsaby@gmail.com>
2022-01-27 09:54:17 +0100

    enfin

--- Portfolio/Sentinelle/Daemon.sh
+++ Portfolio/Sentinelle/Daemon.sh
@@ -4,8 +4,9 @@ var="/mnt/c/Users/aubin/OneDrive/Documents/GitHub/Projets/Portfolio/Sentinelle/t
 number_of_words1=`wc --word < $var`
 
 # méthode simple de comptage de mots
-# fichier qui s'écrase 
-# date et heure à faire
+
+# mettre en uniq les pharses en trop
+# faire en sorte que dès qu'il y a une modification ne pas écrire encore et encore, mais relancer un autre while
 
 echo "Le nombre de mots est : $number_of_words1"
 
@@ -13,17 +14,21 @@ while true
 do
     number_of_words2=`wc --word < $var`
 
-    if test "$number_of_words1" -eq "$number_of_words2"
+    w1=$number_of_words1
+    w2=$number_of_words2
+
+    if test "$w1" -eq "$w2"
     then 
     echo "Le fichier n'a pas été modifié"
     else
-        if test "$number_of_words1" -gt "$number_of_words2"
+        heure=$(date +%H:%M:%S)
+        if test "$w1" -gt "$w2"
         then
-            nb_supp=${number_of_words1}-${number_of_words2}
-            echo "Le fichier a été modifié avec suppression de $nb_supp mots le $date à $heure" > Resu
+            nb_supp=$(($w1-$w2))
+            echo "Le fichier a été modifié avec suppression de $nb_supp mots le `date +%x` à $heure" >> Resu
         else
-            nb_ajout=$number_of_words2-$number_of_words1
-            echo "Le fichier a été modifié avec ajout de $nb_ajout mots le $date à $heure" > Resu
+            nb_ajout=$(($w2-$w1))
+            echo "Le fichier a été modifié avec ajout de $nb_ajout mots le `date +%x` à $heure" >> Resu
         fi
     fi
 
